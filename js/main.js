32cf5b45a3fca95ef9658ccf4e8c81cd83c742e5
aditya-29 <42727919+aditya-29@users.noreply.github.com>
2020-04-21 05:06:42 +0530

    checking

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// import AOS from 'aos';
+// import 'aos/dist/aos.css'; // You can also use <link> for styles
+// // ..
+// AOS.init();
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
